6dbb3bb0f9074c7ef11237558fd5a2d5c81a6399
Sam <sam.wheat@outlook.com>
2023-02-01 20:37:45 -0800

    Add project files.

---
+++ App1/App1.Mobile/MacOS/Main.macOS.cs
@@ -0,0 +1,14 @@
+﻿using AppKit;
+
+namespace App1;
+// This is the main entry point of the application.
+public class EntryPoint
+{
+    static void Main(string[] args)
+    {
+        NSApplication.Init();
+        NSApplication.SharedApplication.Delegate = new App();
+        NSApplication.Main(args);
+    }
+}
+
